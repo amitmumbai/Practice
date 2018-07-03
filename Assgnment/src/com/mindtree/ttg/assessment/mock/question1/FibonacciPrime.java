984856a002e7651bb1c8adaaf4d965f956c39f31
amit <amit.mumbai20@yahoo.com>
2018-07-03 22:26:36 +0530

    FibonacciPrime

--- Assgnment/src/com/mindtree/ttg/assessment/mock/question1/FibonacciPrime.java
+++ Assgnment/src/com/mindtree/ttg/assessment/mock/question1/FibonacciPrime.java
@@ -1,6 +1,4 @@
 package com.mindtree.ttg.assessment.mock.question1;
-
-
 /**
  * A Fibonacci prime is a Fibonacci number that is prime. 
  * Given a list of integer numbers you need to find 
@@ -21,7 +19,42 @@ public class FibonacciPrime {
  * input array is a Fibonacci prime else ‘n’.
  */
 	public String isFibPrime(int[] list) {
-		
+		int max,f,f1=0,f2=1;
+		max=list[0];
+		for(int i=0;i<list.length;i++)
+		{
+			if(max<=list[i])
+			{
+				max=list[i];
+			}
+		}
+		int a[]=new int[max];
+		for(int i=0;i<max;i++)
+		{
+		if(i<=1)
+			f=i;
+		else
+		{
+			f=f1+f2;
+			f1=f2;
+			f2=f;	
+		}
+		if(f>max)
+			{
+			break;
+			}
+		a[i]=f;	
+		}
+		for(int i=0;i<list.length;i++)
+		{
+			for(int j=0;j<a.length;j++)
+			{
+				if(list[i]==a[j])
+				{
+					
+				}
+			}
+		}
 		String output = new String ("nyyny");
 		return output;
 	}
